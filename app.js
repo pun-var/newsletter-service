e3fe9644e2583c60ae4029929fbd4adb84d514fe
ipunith <realpunith@gmail.com>
2022-08-06 20:04:39 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", function(req, res){
+    const firstName = req.body.fName
+    const lastName = req.body.lName
+    const email =   req.body.email
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+
+            }
+
+        ]
+    }
+
+   const jsonData = JSON.stringify(data);
+   const url = "https://us13.api.mailchimp.com/3.0/lists/fc5ad27fde";
+   const options = {
+    method: "POST",
+    auth: "halfspinner:7b5422bac21d6b77de9210cba5db99be-us11"
+   }
+
+   const request = https.request(url, options, function(response){
+       
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    
+   })
+
+   request.write(jsonData);
+   request.end();
+
+})
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+})
+
+app.listen(3000, function(){
+    console.log("server is running on port 3000")
+})
